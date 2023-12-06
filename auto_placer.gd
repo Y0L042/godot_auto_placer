360f02292f128807188f66845191979695c1318c
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-12-06 10:39:13 +0200

    fix for prod

--- auto_placer.gd
+++ auto_placer.gd
@@ -9,7 +9,7 @@ var undo_redo: UndoRedo = UndoRedo.new()
 func _enter_tree() -> void:
 	instance = self
 	print('Auto Placer entered tree')
-	library_ui = preload("res://addons/auto_placer/library_ui.tscn").instantiate()
+	library_ui = preload("res://addons/addon_godot_auto_placer/library_ui.tscn").instantiate()
 	#library_ui.ap_parent = self
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, library_ui)
 
